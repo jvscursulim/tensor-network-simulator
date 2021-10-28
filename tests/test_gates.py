eae6a1bedc2714844009a107b66711fdf65c6f65
nahumsa <nahumsaa@gmail.com>
2021-10-28 17:28:40 -0300

    add all gates
    Added `RX`, `RY`, `RZ` gates.

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -2,10 +2,64 @@ import unittest
 import numpy as np
 import tensornetwork as tn
 
-from tsim.gates import Gate
+from tsim.gates import Gate, RZ, RY, RX
 
 
 class TestGate(unittest.TestCase):
     def test_gate_notimplemented(self):
         with self.assertRaises(NotImplementedError):
             Gate().matrix
+
+
+class TestRZ(unittest.TestCase):
+    def setUp(self) -> None:
+        self.gate = RZ(1.5)
+        self.expected_matrix = np.array(
+            [[np.exp(-1j * 0.75), 0.0], [0.0, np.exp(1j * 0.75)]]
+        )
+
+    def test_RX_matrix(self):
+
+        self.assertTrue(np.allclose(self.gate.matrix, self.expected_matrix))
+
+    def test_RX_tensor(self):
+
+        self.assertTrue(np.allclose(self.gate.node.tensor, self.expected_matrix))
+
+
+class TestRY(unittest.TestCase):
+    def setUp(self) -> None:
+        self.gate = RY(1.5)
+        self.expected_matrix = np.array(
+            [
+                [np.cos(0.75), -np.sin(0.75)],
+                [np.sin(0.75), np.cos(0.75)],
+            ]
+        )
+
+    def test_RY_matrix(self):
+
+        self.assertTrue(np.allclose(self.gate.matrix, self.expected_matrix))
+
+    def test_RY_tensor(self):
+
+        self.assertTrue(np.allclose(self.gate.node.tensor, self.expected_matrix))
+
+
+class TestRX(unittest.TestCase):
+    def setUp(self) -> None:
+        self.gate = RX(1.5)
+        self.expected_matrix = np.array(
+            [
+                [np.cos(0.75), -1.0j * np.sin(0.75)],
+                [-1.0j * np.sin(0.75), np.cos(0.75)],
+            ]
+        )
+
+    def test_RX_matrix(self):
+
+        self.assertTrue(np.allclose(self.gate.matrix, self.expected_matrix))
+
+    def test_RX_tensor(self):
+
+        self.assertTrue(np.allclose(self.gate.node.tensor, self.expected_matrix))
--- tsim/gates.py
+++ tsim/gates.py
@@ -21,3 +21,46 @@ class Gate(ABC):
 
     def __init__(self, *params):
         self.params = list(params)
+
+
+class RZ(Gate):
+    """RZ(theta)
+    Single qubit Z rotation.
+
+    """
+
+    def _matrix(self):
+        theta = self.params[0]
+        return np.array([[np.exp(-1j * theta / 2), 0.0], [0.0, np.exp(1j * theta / 2)]])
+
+
+class RY(Gate):
+    """RY(theta)
+    Single qubit Y rotation.
+
+    """
+
+    def _matrix(self):
+        theta = self.params[0]
+        return np.array(
+            [
+                [np.cos(theta / 2.0), -np.sin(theta / 2.0)],
+                [np.sin(theta / 2.0), np.cos(theta / 2.0)],
+            ]
+        )
+
+
+class RX(Gate):
+    """RX(theta)
+    Single qubit X rotation.
+
+    """
+
+    def _matrix(self):
+        theta = self.params[0]
+        return np.array(
+            [
+                [np.cos(theta / 2.0), -1.0j * np.sin(theta / 2.0)],
+                [-1.0j * np.sin(theta / 2.0), np.cos(theta / 2.0)],
+            ]
+        )
